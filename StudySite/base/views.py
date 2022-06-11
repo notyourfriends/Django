b7de25002429ad0035c6cfab1e9789bbeae0eacb
Fuu <98339035+notyourfriends@users.noreply.github.com>
2022-06-11 11:35:38 +0700

    Dynamic Value

--- StudySite/base/views.py
+++ StudySite/base/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
+rooms = [
+    {'id': 1, 'name':'Lets learn django!'},
+    {'id': 2, 'name':'Lets learn frontend!'},
+    {'id': 3, 'name':'Lets learn backend!'},
+]
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms':rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i ['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
