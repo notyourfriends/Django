47256c5dfe081d8fcd0ae69ee2476c1c23c9ec4d
Fuu <98339035+notyourfriends@users.noreply.github.com>
2022-06-13 19:51:52 +0700

    Update models.py

--- StudySite/base/models.py
+++ StudySite/base/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+    
 
+    def __str__(self):
+        return self.name
 
 class Room(models.Model):
     # host = 
@@ -18,5 +24,11 @@ class Room(models.Model):
         return self.name
 
 class Message(models.Model):
-    # user = 
-    room = 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now = True)
+    created = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.body[0:50]
